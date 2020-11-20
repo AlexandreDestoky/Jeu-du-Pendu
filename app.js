96e0d9257ff01d1476367a8507a3e9a24783cbad
Destoky <adestoky@gmail.com>
2020-11-20 01:42:00 +0100

    Erreur et image
    ajout des erreurs et defilements des images

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 let lettres = document.querySelector(".lettres");
+let erreur = document.querySelector(".erreur");
+let image = document.querySelector(".pendu img");
+let etape = 1;
+let presenceLettre = false;
 
 // let mots = ["sanglier", "tomate", "maison", "ecureil", "chaise"];
 let mots = ["chaises"];
@@ -24,18 +28,36 @@ let divLettres = document.querySelectorAll(".lettres div");
 
 //Quand on tape sur une touche
 document.body.addEventListener("keydown", (e) => {
-
   //si la lettre est la même que celle dans le div on montre la lettre sinon on fait défiler une autre image
   tabLettres.forEach((lettre) => {
     if (e.key == lettre) {
+      presenceLettre = true;
       console.log("passer");
       for (const divLettre of divLettres) {
         if (divLettre.textContent == lettre) {
           divLettre.style.color = "black";
         }
       }
-    } else {
-      
     }
   });
+  if (presenceLettre == false) {
+    affichageErreur(e.key);
+  }
 });
+
+let affichageErreur = (lettre) => {
+  //changement d'image
+  etape++;
+  if (etape < 8) {
+    console.log(etape + " etape");
+    image.src = `img/etape${etape}.png`;
+  } else {
+    console.log("perdu");
+  }
+
+  //creation lettre erronnées
+  let divError = document.createElement("div");
+  divError.textContent = lettre;
+  console.log(divError.textContent)
+  erreur.append(divError);
+};
